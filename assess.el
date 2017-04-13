87118057b3ae0e6542fa5e22050eb44d6efe8baa
Phillip Lord <phillip.lord@russet.org.uk>
2017-04-13 12:09:23 +0100

    v0.4 Release

--- assess.el
+++ assess.el
@@ -6,7 +6,7 @@
 
 ;; Author: Phillip Lord <phillip.lord@russet.org.uk>
 ;; Maintainer: Phillip Lord <phillip.lord@russet.org.uk>
-;; Version: 0.3.2
+;; Version: 0.4
 ;; Package-Requires: ((emacs "24.1")(m-buffer "0.15"))
 
 ;; The contents of this file are subject to the GPL License, Version 3.0.
